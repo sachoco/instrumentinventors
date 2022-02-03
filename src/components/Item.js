6bd8e51121d75d94a109fb96dac89e415c2f07f2
Satopin <sachoco@gmail.com>
2022-02-03 12:05:01 +0100

    Fine tuning thumbnail item hover state

--- src/components/Item.js
+++ src/components/Item.js
@@ -42,34 +42,40 @@ const Item = (props) => {
           />
         </VisibilitySensor>
         <div className="absolute flex items-end z-10 w-full px-5 py-2 bottom-0 border-t-2 border-black bg-overlay transition-all min-h-0 group-hover:min-h-full+2px">
-          <div className="transition-all duration-200 absolute z-20 w-full top-8 left-0  px-5 my-2 opacity-0 delay-0 group-hover:delay-200 group-hover:opacity-100 ">
-            <div className="mt-1 text-xl min-h-16 flex items-end">
+          <div className="transition-all duration-200 absolute z-20 w-full top-0 pt-8 left-0  px-5 my-0 opacity-0 delay-0 group-hover:delay-200 group-hover:opacity-100 h-full-40px overflow-hidden flex items-center">
+            <div className="w-full">
+            <div className="text-xl flex items-end py-2">
+            {/* <div className="text-xl min-h-16 flex items-end py-2"> */}
               <span className="line-clamp-2">{title}</span>
             </div>
-            <div className="border-t-2 line-clamp-2">{meta1}</div>
-            <div className="border-t-2">{meta2}</div>
-            <div className="border-t-2 border-b-2 overflow-hidden overflow-ellipsis whitespace-pre ">
-              {Array.isArray(tag) && tag.length > 0
-                ? tag.map((obj, i) => (
-                    <span key={i}>
-                      {i > 0 && ", "}
-                      {/* <Link to={"/" + archive_base + "/?t=" + obj.id}> */}
-                      <span
-                        onClick={(e) =>
-                          onClickHandler(
-                            e,
-                            "/" + archive_base + "/?t=" + obj.id
-                          )
-                        }
-                        className="hover:underline"
-                      >
-                        {obj.name}
-                      </span>
-                      {/* </Link> */}
+            {meta1.trim() != "" && meta1 != "&nbsp;" && (
+              <div className="line-clamp-3 border-t-2 text-sm py-1">{meta1}</div>
+            )}
+            {meta2.trim() != "" && meta2 != "&nbsp;" && (
+              <div className="border-t-2 text-sm py-1">{meta2}</div>
+            )}
+            {Array.isArray(tag) && tag.length > 0 ? (
+              <div className="line-clamp-2 border-t-2 text-sm py-1 border-b-2 ">
+                {tag.map((obj, i) => (
+                  <span key={i}>
+                    {i > 0 && ", "}
+                    {/* <Link to={"/" + archive_base + "/?t=" + obj.id}> */}
+                    <span
+                      onClick={(e) =>
+                        onClickHandler(e, "/" + archive_base + "/?t=" + obj.id)
+                      }
+                      className="hover:underline"
+                    >
+                      {obj.name}
                     </span>
-                  ))
-                : tag}
-            </div>
+                    {/* </Link> */}
+                  </span>
+                ))}
+              </div>
+            ) : (
+              <div className="border-b-2 " />
+            )}
+          </div>
           </div>
           {/* <div className="transition-all group-hover:text-3xl overflow-hidden whitespace-pre">
 					{title}
@@ -87,9 +93,16 @@ const Item = (props) => {
 					</div> */}
 
             <div
-              className={`relative flex-grow max-w-full mr-2 transition-all group-hover:max-w-0 group-hover:mr-0 duration-2000 ${isOverflow ? "text-sm line-clamp-2 whitespace-normal " : "whitespace-pre overflow-hidden overflow-ellipsis "}`}
+              className={`relative flex-grow max-w-full mr-2 transition-all group-hover:max-w-0 group-hover:mr-0 duration-2000 ${
+                isOverflow
+                  ? "text-sm line-clamp-2 whitespace-normal "
+                  : "whitespace-pre overflow-hidden overflow-ellipsis "
+              }`}
             >
-              <div ref={ref} className="opacity-0 absolute w-full text-base line-clamp-none whitespace-pre overflow-hidden overflow-ellipsis text-red-700">
+              <div
+                ref={ref}
+                className="opacity-0 absolute w-full text-base line-clamp-none whitespace-pre overflow-hidden overflow-ellipsis text-red-700"
+              >
                 {title}
               </div>
               {title}
