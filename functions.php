abef75297278aca20dfd8c1bba44628a13ae2dc3
Satopin <sachoco@gmail.com>
2022-11-10 15:10:50 +0100

    removing address field from checkout

--- functions.php
+++ functions.php
@@ -187,18 +187,20 @@ add_filter( 'woocommerce_checkout_fields' , 'custom_remove_woo_checkout_fields' 
 function custom_remove_woo_checkout_fields( $fields ) {
 
     // remove billing fields
-/*
+
     unset($fields['billing']['billing_first_name']);
     unset($fields['billing']['billing_last_name']);
+/*
     unset($fields['billing']['billing_company']);
     unset($fields['billing']['billing_address_1']);
 */
     unset($fields['billing']['billing_address_2']);
-/*
+
     unset($fields['billing']['billing_city']);
     unset($fields['billing']['billing_postcode']);
     unset($fields['billing']['billing_country']);
     unset($fields['billing']['billing_state']);
+/*
     unset($fields['billing']['billing_phone']);
     unset($fields['billing']['billing_email']);
 */
