84573d8751ce52ccd629ee2042b840223394c63d
Satopin <sachoco@gmail.com>
2024-06-03 21:50:26 +0200

    donation link

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -33,8 +33,8 @@ const Footer = () => (
       <p>
         <a
           className="hover:underline"
-          href="/product-category/donation/"
-          target="_self"
+          href="https://shop.instrumentinventors.org/product/donation/"
+          target="_blank"
         >
           click for more info
         </a>
