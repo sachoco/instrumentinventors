28f0e9390033e2cabdb3b754de9740fc5de859c2
Satopin <sachoco@gmail.com>
2024-06-26 21:09:10 +0200

    Deactivating WooCommerce

--- functions.php
+++ functions.php
@@ -36,8 +36,8 @@ function theme_scripts() {
 	 * @var \WPackio\Enqueue
 	 */
 	global $enqueue;
-	if(!is_woocommerce()&&!is_product()&&!is_cart()&&!is_checkout()&&!is_account_page()&&!is_admin()):
-
+	// if(!is_woocommerce()&&!is_product()&&!is_cart()&&!is_checkout()&&!is_account_page()&&!is_admin()):
+	if(!is_admin()):
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'lightbox2' );
 		wp_enqueue_style( 'lightbox2' );
@@ -182,49 +182,49 @@ function bidirectional_acf_update_value( $value, $post_id, $field  ) {
 add_filter('acf/update_value/name=related_posts', 'bidirectional_acf_update_value', 10, 3);
 
 
-add_filter( 'woocommerce_checkout_fields' , 'custom_remove_woo_checkout_fields' );
+// add_filter( 'woocommerce_checkout_fields' , 'custom_remove_woo_checkout_fields' );
 
-function custom_remove_woo_checkout_fields( $fields ) {
+// function custom_remove_woo_checkout_fields( $fields ) {
 
-    // remove billing fields
-/*
-    unset($fields['billing']['billing_first_name']);
-    unset($fields['billing']['billing_last_name']);
-*/
-    unset($fields['billing']['billing_company']);
-    unset($fields['billing']['billing_address_1']);
+//     // remove billing fields
+// /*
+//     unset($fields['billing']['billing_first_name']);
+//     unset($fields['billing']['billing_last_name']);
+// */
+//     unset($fields['billing']['billing_company']);
+//     unset($fields['billing']['billing_address_1']);
 
-    unset($fields['billing']['billing_address_2']);
+//     unset($fields['billing']['billing_address_2']);
 
-    unset($fields['billing']['billing_city']);
-    unset($fields['billing']['billing_postcode']);
-    unset($fields['billing']['billing_country']);
-    unset($fields['billing']['billing_state']);
-/*
-    unset($fields['billing']['billing_phone']);
-    unset($fields['billing']['billing_email']);
-*/
+//     unset($fields['billing']['billing_city']);
+//     unset($fields['billing']['billing_postcode']);
+//     unset($fields['billing']['billing_country']);
+//     unset($fields['billing']['billing_state']);
+// /*
+//     unset($fields['billing']['billing_phone']);
+//     unset($fields['billing']['billing_email']);
+// */
 
-    // remove shipping fields
-/*
-    unset($fields['shipping']['shipping_first_name']);
-    unset($fields['shipping']['shipping_last_name']);
-    unset($fields['shipping']['shipping_company']);
-    unset($fields['shipping']['shipping_address_1']);
-*/
-    unset($fields['shipping']['shipping_address_2']);
-/*
-    unset($fields['shipping']['shipping_city']);
-    unset($fields['shipping']['shipping_postcode']);
-    unset($fields['shipping']['shipping_country']);
-    unset($fields['shipping']['shipping_state']);
-*/
+//     // remove shipping fields
+// /*
+//     unset($fields['shipping']['shipping_first_name']);
+//     unset($fields['shipping']['shipping_last_name']);
+//     unset($fields['shipping']['shipping_company']);
+//     unset($fields['shipping']['shipping_address_1']);
+// */
+//     unset($fields['shipping']['shipping_address_2']);
+// /*
+//     unset($fields['shipping']['shipping_city']);
+//     unset($fields['shipping']['shipping_postcode']);
+//     unset($fields['shipping']['shipping_country']);
+//     unset($fields['shipping']['shipping_state']);
+// */
 
-    // remove order comment fields
-//     unset($fields['order']['order_comments']);
+//     // remove order comment fields
+// //     unset($fields['order']['order_comments']);
 
-    return $fields;
-}
+//     return $fields;
+// }
 
 
 /**
--- index.php
+++ index.php
@@ -1,27 +1,3 @@
 <?php get_header(); ?>
-<?php
-if(!is_woocommerce()&&!is_product()&&!is_cart()&&!is_checkout()&&!is_account_page()):
-?>
 <div id='root'></div>
-<?php else: ?>
-<div id="react-header"></div>
-<div class="flex">
-  <div id="react-sidemenu"></div>
-    <div class="relative flex-grow z-10 pt-14 lg:pt-24 lg:ml-24 overflow-hidden">
-
-    <?php
-    if ( have_posts() ) {
-    	// Load posts loop.
-    	while ( have_posts() ) {
-    		the_post();
-        echo '<div class="px-8 py-10 lg:p-24 woocommerce-content">';
-        the_content();
-        echo '</div>';
-    	}
-    }
-    ?>
-    <div id="react-footer"></div>
-  </div>
-</div>
-<?php endif; ?>
 <?php get_footer(); ?>
